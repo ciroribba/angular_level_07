99b895085017e86648f4ddf1d81ae0696846d7bb
ciroribba <ciro.ribba@gmail.com>
2025-12-29 18:38:39 -0300

    basic arquitecture

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { HousesPage } from './pages/houses-page/houses-page';
+import { MarkersPage } from './pages/markers-page/markers-page';
+import { FullscreenMapPage } from './pages/fullscreen-map-page/fullscreen-map-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: FullscreenMapPage,
+        title: 'Fullscreen Map'
+    },
+    {
+        path: 'houses',
+        component: HousesPage,
+        title: 'Houses'
+    },
+    {
+        path: 'markers',
+        component: MarkersPage,
+        title: 'Markers'
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
